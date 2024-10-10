a550143681cf2cddfc69834fe3c3f0d0832ce005
unknown <me.leandrop@gmail.com>
2024-10-10 14:44:51 -0300

    backUrl constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,5 @@ export const cloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME as string;
 export const apiKey = import.meta.env.VITE_CLOUDINARY_API_KEY as string;
 export const apiSecret = import.meta.env.VITE_CLOUDINARY_API_SECRET as string;
 export const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET as string;
+
+export const URL_BACK = "http://localhost:3000/api" as string;
