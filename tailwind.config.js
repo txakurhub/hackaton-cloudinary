eb3d28bddbd4be0a8b358f35c59bdf222d273a66
unknown <me.leandrop@gmail.com>
2024-10-13 16:06:49 -0300

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        celeste: "#0293CE",
+        verde: "#2D9E55",
+        naranja: "#EB943E",
+        violeta: "#C32188",
+      },
+      fontFamily: {
+        title: ["Old Celtiberians", "sans-serif"],
+        body: ["ChelseaMarket", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
